0ad213fcb03c66730d27a426b85865d6b07cb673
Drebakare <bakaredmlr@gmail.com>
2022-07-14 02:25:57 +0100

    creates wallet data

---
+++ src/data/WalletData.ts
@@ -0,0 +1,27 @@
+import { Coinbase } from "@/components/icons/coinbase";
+import { Metamask } from "@/components/icons/metamask";
+import { Trevor } from "@/components/icons/trevor";
+import { WalletConnect } from "@/components/icons/walletconnect";
+
+export const WALLET_DATA = [
+    {
+        id: 1,
+        name: "Metamask",
+        icon: Metamask
+    },
+    {
+        id: 2,
+        name: "Wallet Connect",
+        icon: WalletConnect
+    },
+    {
+        id: 3,
+        name: "Coinbase",
+        icon: Coinbase
+    },
+    {
+        id: 4,
+        name: "Trevor",
+        icon: Trevor
+    }
+]
