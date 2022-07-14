1bb3f5c829774321903803417601b2f253625fb2
Drebakare <bakaredmlr@gmail.com>
2022-07-14 02:10:36 +0100

    fixes lint error

--- src/data/NetworkData.ts
+++ src/data/NetworkData.ts
@@ -1,21 +1,21 @@
-import { Bsc } from "@/components/icons/bsc";
-import { Ethereum } from "@/components/icons/ethereum";
-import { Polygon } from "@/components/icons/polygon";
+import { Bsc } from '@/components/icons/bsc'
+import { Ethereum } from '@/components/icons/ethereum'
+import { Polygon } from '@/components/icons/polygon'
 
 export const NETWORK_DATA = [
-    {
-        id: 1,
-        name: "Ethereum",
-        icon: Ethereum
-    },
-    {
-        id: 2,
-        name: "polygon",
-        icon: Polygon,
-    },
-    {
-        id: 3,
-        name: "Bsc",
-        icon: Bsc
-    }
+  {
+    id: 1,
+    name: 'Ethereum',
+    icon: Ethereum,
+  },
+  {
+    id: 2,
+    name: 'polygon',
+    icon: Polygon,
+  },
+  {
+    id: 3,
+    name: 'Bsc',
+    icon: Bsc,
+  },
 ]
