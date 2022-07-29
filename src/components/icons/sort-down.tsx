1e755e175b5998edff67f89e9b4d15973434c858
anubra266 <anubra266@gmail.com>
2022-07-29 10:12:13 +0100

    fixes

---
+++ src/components/icons/sort-down.tsx
@@ -0,0 +1,22 @@
+import { Icon, IconProps } from '@chakra-ui/react'
+import React from 'react'
+
+export const SortDown = (props: IconProps) => {
+  return (
+    <Icon
+      width="10px"
+      height="16px"
+      viewBox="0 0 10 16"
+      fill="none"
+      stroke="currentColor"
+      strokeOpacity="0.92"
+      strokeWidth="1.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path d="M4.83681 15.1637V1.5459" />
+      <path d="M8.91727 11.0674L4.8395 15.164L0.761719 11.0674" />
+    </Icon>
+  )
+}
