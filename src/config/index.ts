723935fb5c3c47c57e6bc86899f174d3ab15b54f
Drebakare <bakaredmlr@gmail.com>
2022-08-19 12:21:16 +0100

    adds default value

--- src/config/index.ts
+++ src/config/index.ts
@@ -12,13 +12,17 @@ const config = {
     process.env.NEXT_PUBLIC_BLOCK_EXPLORER_BASE_URL ||
     'https://goerli.etherscan.io/',
   infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
-  alchemyApiKey: String(process.env.NEXT_PUBLIC_ALCHEMY_API_KEY),
+  alchemyApiKey:
+    String(process.env.NEXT_PUBLIC_ALCHEMY_API_KEY) ||
+    'dgF6IdB0eWQgVz2m5Vo_3ei49RqAGAw5',
   alchemyChain: process.env.NEXT_PUBLIC_ALCHEMY_CHAIN || 'goerli',
   graphqlUrl:
     process.env.NEXT_PUBLIC_EP_API || 'https://api.dev.braindao.org/graphql',
   chainId: process.env.NEXT_PUBLIC_CHAIN_ID || '5',
   chainName: process.env.NEXT_PUBLIC_CHAIN_NAME || 'goerli',
-  ensRPC: String(process.env.NEXT_PUBLIC_ENS_RPC),
+  ensRPC:
+    String(process.env.NEXT_PUBLIC_ENS_RPC) ||
+    'https://eth-goerli.g.alchemy.com/v2/dgF6IdB0eWQgVz2m5Vo_3ei49RqAGAw5',
 }
 
 export default config
