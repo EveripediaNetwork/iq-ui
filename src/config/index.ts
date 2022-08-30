386d68e5df1efca7d040e374c950e2118f4bd317
anubra266 <anubra266@gmail.com>
2022-08-30 22:51:01 +0100

    token balances

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,29 +1,3 @@
-// const config = {
-//   iqAddress:
-//     process.env.NEXT_PUBLIC_IQ_ADDRESS ||
-//     '0xb9638272ad6998708de56bbc0a290a1de534a578',
-//   wikiContractAddress:
-//     process.env.NEXT_PUBLIC_WIKI_CONTRACT_ADDRESS ||
-//     '0x94bb4c72252d0ae7a98b2b0483Dc4145C0C79059',
-//   blockExplorerUrl:
-//     process.env.NEXT_PUBLIC_BLOCK_EXPLORER_BASE_URL ||
-//     'https://mumbai.polygonscan.com',
-//   pinataBaseUrl:
-//     process.env.NEXT_PUBLIC_PINATA_GATEWAY_BASE_URL ||
-//     'https://ipfs.everipedia.org/ipfs/',
-//   infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
-//   alchemyApiKey: String(process.env.NEXT_PUBLIC_ALCHEMY_API_KEY),
-//   alchemyChain: process.env.NEXT_PUBLIC_ALCHEMY_CHAIN_POLYGON || 'maticmum',
-//   graphqlUrl:
-//     process.env.NEXT_PUBLIC_EP_API || 'https://api.dev.braindao.org/graphql',
-//   chainId: process.env.NEXT_PUBLIC_CHAIN_ID || '80001',
-//   chainName: process.env.NEXT_PUBLIC_CHAIN_NAME || 'Mumbai',
-//   ensRPC: String(process.env.NEXT_PUBLIC_ENS_RPC_POLYGON),
-//   publicDomain: String(process.env.NEXT_PUBLIC_DOMAIN),
-// }
-
-// export default config
-
 const config = {
   iqAddress:
     process.env.NEXT_PUBLIC_IQ_ADDRESS ||
