579b3792821bd94f555ff7bf210631fcd7c5f7f5
Drebakare <bakaredmlr@gmail.com>
2022-09-07 23:39:11 +0100

    designs raffle page

--- src/data/RaffleData.ts
+++ src/data/RaffleData.ts
@@ -1,20 +1,23 @@
 export const RAFFLE_DATA = [
-    {
-        id: 1,
-        title: "Shinsekai",
-        body: "Shinsekai is the first 3D Manga NFT collection that aims to be the pinnacle of the web3 Manga community.Together we will build a New World, in other words Shinsekai 新世界.",
-        madeBy: "ShinsekaiPortal",
-    },
-    {
-        id: 2,
-        title: "Zipcy's SuperNormal",
-        body: "Consisting of over 1000 traits, these 8,888 Generative NFTs on the Ethereum blockchain represent individualism by portraying elements of the diverse and unique. This is done by allowing the singular, inventive works to be composed of six comprehensive styles, encompassing a variety of human identities and emotions. ",
-        madeBy: "SNTeam",
-    },
-    {
-        id: 3,
-        title: "Juicy J's Journey NFT Drop",
-        body: "Juicy J and ASTERISK have collaborated with Everipedia (IQ) to launch Juicy J’s Journey, a limited collection of NFTs to celebrate the launch of Juicy J’s latest album The Hustle Still Continues.The collection uses blockchain technology, digital artwork, scenes from Juicy’s music videos and metadata.",
-        madeBy: "therealjuicyj",
-    }
+  {
+    id: 1,
+    title: 'Shinsekai',
+    body: 'Shinsekai is the first 3D Manga NFT collection that aims to be the pinnacle of the web3 Manga community.Together we will build a New World, in other words Shinsekai 新世界.',
+    madeBy: 'ShinsekaiPortal',
+    imageUrl: "/images/shine.png"
+  },
+  {
+    id: 2,
+    title: "Zipcy's SuperNormal",
+    body: 'Consisting of over 1000 traits, these 8,888 Generative NFTs on the Ethereum blockchain represent individualism by portraying elements of the diverse and unique. This is done by allowing the singular, inventive works to be composed of six comprehensive styles, encompassing a variety of human identities and emotions. ',
+    madeBy: 'SNTeam',
+    imageUrl: "/images/zipcy.png"
+  },
+  {
+    id: 3,
+    title: "Juicy J's Journey NFT Drop",
+    body: 'Juicy J and ASTERISK have collaborated with Everipedia (IQ) to launch Juicy J’s Journey, a limited collection of NFTs to celebrate the launch of Juicy J’s latest album The Hustle Still Continues.The collection uses blockchain technology, digital artwork, scenes from Juicy’s music videos and metadata.',
+    madeBy: 'therealjuicyj',
+    imageUrl: "/images/juicy.png"
+  },
 ]
---
+++ src/pages/dashboard/raffles.tsx
@@ -0,0 +1,76 @@
+import { DashboardLayout } from '@/components/dashboard/layout'
+import { RAFFLE_DATA } from '@/data/RaffleData'
+import {
+  Image,
+  Flex,
+  Heading,
+  SimpleGrid,
+  Text,
+  Stack,
+  Divider,
+  Box
+} from '@chakra-ui/react'
+import React from 'react'
+
+const Raffles = () => {
+  return (
+    <DashboardLayout>
+      <Flex direction="column" gap="6" pt="4" pb="20">
+        <Flex direction="column" gap="2">
+          <Heading fontWeight="bold" fontSize={{ md: 'xl', lg: '2xl' }}>
+            IQ Raffles
+          </Heading>
+          <Text fontSize={{ base: 'sm', md: 'md' }} color="fadedText">
+            Follow all the raffles made within the IQ world.
+          </Text>
+        </Flex>
+        <SimpleGrid
+          mt="4"
+          columns={{ base: 1, md: 2 }}
+          spacingX={4}
+          spacingY={8}
+        >
+          {RAFFLE_DATA.map((raffle, index) => (
+              <Flex key={index} direction="column" rounded="lg" maxW="full">
+                <Image
+                  src={raffle.imageUrl}
+                  loading="lazy"
+                  width="auto"
+                  height="175px"
+                  fit="cover"
+                />
+                <Stack
+                  px={{ base: '2.5', lg: '3' }}
+                  pt={{ base: '3', md: '2.5', lg: '4' }}
+                  pb={{ base: '4', md: '2', lg: '6' }}
+                  shadow="md"
+                >
+                  <Text fontWeight="medium" fontSize="lg">
+                    {raffle.title}
+                  </Text>
+                  <Divider orientation="horizontal"/>
+                  <Text  py={2} fontSize="sm">
+                    {raffle.body}
+                  </Text>
+                  <Text fontWeight="medium" py={2} fontSize="sm">
+                    made by: {raffle.madeBy}
+                  </Text>
+                  <Divider orientation="horizontal"/>
+                  <Box display='flex' mt='2' alignItems='center'>
+                        <Text as='span' color='brandText' fontSize='sm'>
+                            Snapshot
+                        </Text>
+                        <Text as='span' ml='4' color='brandText' fontSize='sm'>
+                            Onchain Resuls
+                        </Text>
+                    </Box>
+                </Stack>
+              </Flex>
+          ))}
+        </SimpleGrid>
+      </Flex>
+    </DashboardLayout>
+  )
+}
+
+export default Raffles
