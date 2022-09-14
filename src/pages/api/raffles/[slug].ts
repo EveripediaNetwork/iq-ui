57e920fd0da60ff496471db0a34afb5e6608904e
Drebakare <bakaredmlr@gmail.com>
2022-09-14 11:36:28 +0100

    removes api folder

--- src/pages/api/raffles/[slug].ts
+++
@@ -1,8 +0,0 @@
-import { RAFFLE_DATA } from '@/data/RaffleData'
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { slug } = req.query
-  const raffle = RAFFLE_DATA.find(r => r.slug === slug)
-  res.status(200).json(raffle)
-}
--- src/pages/api/raffles/index.ts
+++
@@ -1,6 +0,0 @@
-import { RAFFLE_DATA } from '@/data/RaffleData'
-import { NextApiRequest, NextApiResponse } from 'next'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(RAFFLE_DATA)
-}
