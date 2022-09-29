b7c33d28292a53906652831b244da474f933195b
Drebakare <bakaredmlr@gmail.com>
2022-09-29 12:28:12 +0100

    adds watch to balace, total lock iq and lock end date

--- src/hooks/useLockOverview.ts
+++ src/hooks/useLockOverview.ts
@@ -36,6 +36,7 @@ export const useLockOverview = () => {
     functionName: 'locked__end',
     args: [address],
     overrides: { gasLimit: DEFAULT_GAS_LIMIT },
+    watch: true
   })
 
   const {
@@ -47,6 +48,7 @@ export const useLockOverview = () => {
     functionName: 'locked',
     args: [address],
     overrides: { gasLimit: DEFAULT_GAS_LIMIT },
+    watch: true
   })
 
   const getTotalHiiqSupply = () => {
