14fa06c6194d0bfcadc38d9b397e36d5cdbbd31c
Drebakare <bakaredmlr@gmail.com>
2022-11-26 13:24:38 +0100

    set up api for token details

---
+++ src/pages/api/token-details.ts
@@ -0,0 +1,32 @@
+import config from '@/config'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type ResponseData = {
+  status: boolean
+  message: string
+  response?: any
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>,
+) {
+  if (!req.body) {
+    return res
+      .status(400)
+      .json({ status: false, message: 'Token address and chain id are needed' })
+  }
+  const { tokenAddress, chain } = JSON.parse(req?.body)
+  const url = `https://pro-openapi.debank.com/v1/user/token_list?id=${tokenAddress}&chain_id=${chain}&is_all=true`
+  const result = await fetch(url, {
+    headers: {
+      Accept: 'application/json',
+      Accesskey: `${config.debankApiKey}`,
+    },
+  })
+  return res.status(200).json({
+    response: await result.json(),
+    status: true,
+    message: 'token details successfully fetched',
+  })
+}
