77670944652842beb8807f4f4a4080c5f327b07d
Drebakare <bakaredmlr@gmail.com>
2023-01-25 15:04:45 +0100

    redesigns voting control

--- src/components/gauges/votingControls.tsx
+++ src/components/gauges/votingControls.tsx
@@ -16,7 +16,8 @@ import {
   Button,
   useToast,
   Divider,
-  HStack
+  HStack,
+  chakra
 } from '@chakra-ui/react'
 import { useGaugeCtrl } from '@/hooks/useGaugeCtrl'
 import { useAppSelector } from '@/store/hook'
@@ -57,7 +58,14 @@ const VotingControls = () => {
 
   return (
     <Flex direction="column">
-      <Box border="solid 1px" borderColor="divider" rounded="lg" pt="4" px="2" mb={6}>
+      <Box
+        border="solid 1px"
+        borderColor="divider"
+        rounded="lg"
+        pt="4"
+        px="2"
+        mb={6}
+      >
         <Text ml="2">Voting Details</Text>
         <Divider w="140px" borderColor="divider" my={1} />
         <Text fontSize="sm" textAlign="center" my={14} fontWeight="thin">
@@ -66,6 +74,17 @@ const VotingControls = () => {
         </Text>
       </Box>
       <Flex direction="column" px={2}>
+         <Box>
+          <Flex
+            flexWrap="wrap"
+            flexDirection="row"
+            justifyContent="space-between"
+            mb={1}
+          >
+            <Text fontWeight="bold" fontSize="sm">Guages: <chakra.span fontSize="xs" fontWeight="normal"> % of weight to allocate:</chakra.span></Text>
+            <Text textAlign="left">{weightToAllocate}</Text>
+          </Flex>
+        </Box>
         <Slider
           isDisabled={unusedRaw === 0 || !canVote}
           aria-label="slider-ex-2"
@@ -80,28 +99,28 @@ const VotingControls = () => {
           <SliderThumb />
         </Slider>
         <HStack mt={6}>
-        <NumberInput
-          isDisabled={unusedRaw === 0 || !canVote}
-          defaultValue={0}
-          maxW={20}
-          min={0}
-          max={100}
-          value={weightToAllocate}
-          onChange={(_, value: number) => setWeightToAllocate(value)}
-        >
-          <NumberInputField />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-        <Button
-          onClick={handleVote}
-          disabled={isVoting || !canVote || weightToAllocate === 0}
-          ml={4}
-        >
-          {isVoting ? 'Loading' : 'Vote'}
-        </Button>
+          <NumberInput
+            isDisabled={unusedRaw === 0 || !canVote}
+            defaultValue={0}
+            maxW={20}
+            min={0}
+            max={100}
+            value={weightToAllocate}
+            onChange={(_, value: number) => setWeightToAllocate(value)}
+          >
+            <NumberInputField />
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+          <Button
+            onClick={handleVote}
+            disabled={isVoting || !canVote || weightToAllocate === 0}
+            ml={4}
+          >
+            {isVoting ? 'Loading' : 'Vote'}
+          </Button>
         </HStack>
       </Flex>
       {currentGauge !== undefined ? (
