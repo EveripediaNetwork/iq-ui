b55e245104bc7d27d84ddca4a2be105521c2c2b2
Drebakare <bakaredmlr@gmail.com>
2023-02-06 15:45:32 +0100

    renames API

--- src/utils/stats-data.ts
+++ src/utils/stats-data.ts
@@ -13,18 +13,18 @@ import { formatContractResult } from './LockOverviewUtils'
 
 const everipediaBaseApiEndpoint = 'https'
 
-const getEosSupplyUsingBlockAPI = async () => {
+const getEosSupplyUsingGreymassAPI = async () => {
   try {
     const response = await axios.post(
       'https://eos.greymass.com/v1/chain/get_table_rows',
       '{"json":true,"code":"everipediaiq","scope":"IQ","table":"stat","index_position":1,"key_type":"","limit":"1"}',
       {
-          headers: {
-              'Content-Type': 'application/x-www-form-urlencoded'
-          }
-      }
-    );
-    const result = response.data.rows[0].supply.split(" ")
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      },
+    )
+    const result = response.data.rows[0].supply.split(' ')
     return result[0]
   } catch (err) {
     console.log(getError(err))
@@ -34,12 +34,14 @@ const getEosSupplyUsingBlockAPI = async () => {
 
 const getEosSupply = async () => {
   try {
-    const response = await fetch('https://www.api.bloks.io/tokens/IQ-eos-everipediaiq',)
+    const response = await fetch(
+      'https://www.api.bloks.io/tokens/IQ-eos-everipediaiq',
+    )
     const result = await response.json()
     return result[0].supply.circulating
   } catch (err) {
     console.log(getError(err))
-    const result = await getEosSupplyUsingBlockAPI()
+    const result = await getEosSupplyUsingGreymassAPI()
     return result
   }
 }
