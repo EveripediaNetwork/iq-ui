07cdc8a2e668d2bdbaa769f26f041a03b856be87
Drebakare <bakaredmlr@gmail.com>
2023-02-16 16:47:01 +0100

    moves nftFarmAddresses to the constant folder

--- src/components/GaugesInitiator.tsx
+++ src/components/GaugesInitiator.tsx
@@ -1,11 +1,9 @@
+import { nftFarmAddresses } from '@/data/GaugeData'
 import React from 'react'
 import GaugesSetter from './GaugesSetter'
 
 const GaugesInitiator = () => {
-  const nftFarmAddresses = [
-    '0x10f0d0409a6c1626A85075E00d4FDa8733d5236C',
-    '0xfD8f558D4AB0c5dD3D240c780B549F298420A27A',
-  ]
+
   return (
     <>
       {nftFarmAddresses.map(addr => (
