42fd46315b4d6291deb5b4c893f2e0ef79ab76bf
Drebakare <bakaredmlr@gmail.com>
2023-02-23 13:06:11 +0100

    removes default value

--- src/store/slices/nftFarm-slice.ts
+++ src/store/slices/nftFarm-slice.ts
@@ -10,7 +10,7 @@ const initialState: InitialStateType = {
   stakingTypes: nftFarmAddresses,
   currentStakingAddress: nftFarmAddresses
     ? nftFarmAddresses[0]
-    : '0xfD8f558D4AB0c5dD3D240c780B549F298420A27A',
+    : '',
 }
 
 const nftFarmSlice = createSlice({
