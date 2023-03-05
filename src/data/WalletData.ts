51110b078a4decc4c38c1503a97781738be4fcf6
Drebakare <bakaredmlr@gmail.com>
2023-03-05 12:44:46 +0100

    adds wallet logos type

--- src/data/WalletData.ts
+++ src/data/WalletData.ts
@@ -2,5 +2,6 @@ import { Coinbase } from '@/components/icons/coinbase'
 import { Metamask } from '@/components/icons/metamask'
 import { Trevor } from '@/components/icons/trevor'
 import { WalletConnect } from '@/components/icons/walletconnect'
+import { IconProps } from '@chakra-ui/accordion/node_modules/@chakra-ui/icon'
 
-export const WALLET_LOGOS = [Metamask, WalletConnect, Coinbase, Trevor]
+export const WALLET_LOGOS: ((props: IconProps) => JSX.Element)[] = [Metamask, WalletConnect, Coinbase, Trevor]
