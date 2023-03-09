32b6446236135c967d817435ba5083d867f3472a
Drebakare <bakaredmlr@gmail.com>
2023-03-09 22:44:08 +0100

    fixes lint error

--- src/app/dashboards/swap/page.tsx
+++ src/app/dashboards/swap/page.tsx
@@ -3,20 +3,18 @@ import type { Metadata, NextPage } from 'next'
 import SwapPage from '@/components/client/SwapPage'
 
 export const metadata: Metadata = {
-    title: "Swap Page",
+  title: 'Swap Page',
+  description:
+    'Get involved in the IQ Ecosystem and swap the IQ token easily across different exchange platforms.',
+  openGraph: {
+    title: 'IQ Swap platforms',
     description:
       'Get involved in the IQ Ecosystem and swap the IQ token easily across different exchange platforms.',
-    openGraph:{
-        title: 'IQ Swap platforms',
-        description:
-          'Get involved in the IQ Ecosystem and swap the IQ token easily across different exchange platforms.',
-    },
-  }
+  },
+}
 
 const Swap: NextPage = () => {
-  return (
-    <SwapPage/>
-  )
+  return <SwapPage />
 }
 
 export default Swap
