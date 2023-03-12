0fe4665de3838e40d9a837e2262d8d1582022312
Drebakare <bakaredmlr@gmail.com>
2023-03-12 17:34:24 +0100

    reloacates stats page

---
+++ src/app/dashboards/stats/page.tsx
@@ -0,0 +1,15 @@
+import StatsPage from '@/components/client/StatsPage'
+import { Metadata } from 'next'
+import React from 'react'
+
+export const metadata: Metadata = {
+  title: 'Stats Page',
+  description: 'The numbers behind the IQ ecosystem.',
+  openGraph: {
+    title: 'IQ Stats',
+    description: 'The numbers behind the IQ ecosystem.',
+  },
+}
+export const page = () => {
+  return <StatsPage/>
+}
