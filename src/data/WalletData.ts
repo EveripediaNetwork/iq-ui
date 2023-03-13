e4ad5cfdd238b5f1717b751d336522a7f81f1dbd
Drebakare <bakaredmlr@gmail.com>
2023-03-13 13:06:31 +0100

    fixes type issue

--- src/data/WalletData.ts
+++ src/data/WalletData.ts
@@ -4,4 +4,9 @@ import { Trevor } from '@/components/icons/trevor'
 import { WalletConnect } from '@/components/icons/walletconnect'
 import { IconProps } from '@chakra-ui/react'
 
-export const WALLET_LOGOS: ((props: IconProps) => JSX.Element)[] = [Metamask, WalletConnect, Coinbase, Trevor]
+export const WALLET_LOGOS: ((props: IconProps) => JSX.Element)[] = [
+  Metamask,
+  WalletConnect,
+  Coinbase,
+  Trevor,
+]
