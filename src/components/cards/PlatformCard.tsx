d90a5ecd56b2041141d652b5ef884c5e7ed77035
Drebakare <bakaredmlr@gmail.com>
2023-04-29 09:57:47 +0100

    fixes lint error

--- src/components/cards/PlatformCard.tsx
+++ src/components/cards/PlatformCard.tsx
@@ -15,31 +15,31 @@ const PlatformCard = ({
   return (
     <LinkBox>
       <Box w={{ base: '132', md: '176' }} textAlign="center">
-      <Box
-        h={{ base: '92', md: '122' }}
-        justifyContent="center"
-        alignItems="center"
-        display="flex"
-        border="solid 1px "
-        borderColor="divider"
-        rounded="lg"
-        _hover={{
-          bg: 'divider',
-          color: 'dimmedText',
-        }}
-        cursor="pointer"
-      >
-        {/* <a href={route} target="_blank" rel="noreferrer">
+        <Box
+          h={{ base: '92', md: '122' }}
+          justifyContent="center"
+          alignItems="center"
+          display="flex"
+          border="solid 1px "
+          borderColor="divider"
+          rounded="lg"
+          _hover={{
+            bg: 'divider',
+            color: 'dimmedText',
+          }}
+          cursor="pointer"
+        >
+          {/* <a href={route} target="_blank" rel="noreferrer">
          
         </a> */}
-        <LinkOverlay href={route} target="_blank">
-          <Icon as={icon} boxSize="14" _light={{ color: 'black' }} />
-        </LinkOverlay>
+          <LinkOverlay href={route} target="_blank">
+            <Icon as={icon} boxSize="14" _light={{ color: 'black' }} />
+          </LinkOverlay>
+        </Box>
+        <Text fontSize={{ base: 'sm', lg: 'lg' }} mt={2} fontWeight="bold">
+          {name}
+        </Text>
       </Box>
-      <Text fontSize={{ base: 'sm', lg: 'lg' }} mt={2} fontWeight="bold">
-        {name}
-      </Text>
-    </Box>
     </LinkBox>
   )
 }
