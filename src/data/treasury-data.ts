a4f2608443239a4ebba7ce6fdb80797b4267449a
OleanjiKingCode <holamilean56@gmail.com>
2023-06-28 21:24:27 +0100

    added cvxFXS to treasury page

--- src/data/treasury-data.ts
+++ src/data/treasury-data.ts
@@ -94,7 +94,7 @@ export const TOKENS: TokensType = {
   },
 }
 
-export const tokenIds = Object.values(TOKENS).map(tok => tok.id)
+export const tokenIds = Object.values(TOKENS).map((tok) => tok.id)
 
 export const PIE_CHART_COLORS: {
   [key: string]: { light: string; dark: string }
