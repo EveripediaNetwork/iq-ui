020d0d34522c849567991847c453f3e5f4800031
OleanjiKingCode <holamilean56@gmail.com>
2023-07-04 18:19:58 +0100

    lock slider fix, getting provider using publicClient

--- src/components/elements/Slider/LockSlider.tsx
+++ src/components/elements/Slider/LockSlider.tsx
@@ -92,7 +92,7 @@ const LockSlider = ({
             w={{ base: 'full', md: 330, lg: 250 }}
             defaultValue={[lockPeriod]}
             value={[lockPeriod]}
-            onChange={value => updateLockPeriod(value[0])}
+            onChange={(value) => updateLockPeriod(value[0])}
             step={1}
             max={remainingLockablePeriod}
           >
@@ -115,7 +115,7 @@ const LockSlider = ({
             <Input
               value={lockPeriod}
               w={{ base: 'full', md: '10' }}
-              onChange={e => updateLockPeriod(e.target.value)}
+              onChange={(e) => updateLockPeriod(e.target.value)}
               color="grayText4"
               disabled={!isConnected}
               bg="lightCard"
