878ad2d1077746dea7d45c3232c9b2d77814ddf8
Damilare Bakare <bakaredmlr@gmail.com>
2023-07-05 17:41:05 +0100

    reworks tooltip

--- src/components/dashboard/CustomTooltip.tsx
+++ src/components/dashboard/CustomTooltip.tsx
@@ -1,17 +1,38 @@
 import React from 'react'
 import { Dict } from '@chakra-ui/utils'
+import { Box, Text } from '@chakra-ui/layout'
 
 const CustomTooltip = ({
   active,
   payload,
-  tooltipTitle = 'Price',
-  symbol = '$',
+  isPrice=true
 }: Dict) => {
   if (active && payload && payload.length) {
     return (
-      <p>
-        <b>{tooltipTitle}:</b> {`${symbol}${payload[0].value.toFixed(6)}`}
-      </p>
+      <Box position="relative" p={2} rounded="lg">
+        <Box
+          position="absolute"
+          top={0} 
+          left={0} 
+          right={0} 
+          bottom={0} 
+          bg="white" 
+          rounded="lg"
+          opacity={0.3}
+        />
+        {isPrice ? (
+          <>
+            <b>Price:</b> {`$${payload[0].value.toFixed(6)}`}
+          </>
+        ) : (
+          <>
+            <Text fontWeight="bold" fontSize="sm">
+              IQ Staked: 1,200 IQ
+            </Text>
+            <Text fontSize="sm">July 3, 2023</Text>
+          </>
+        )}
+      </Box>
     )
   }
 
--- src/components/dashboard/StakeGraph.tsx
+++ src/components/dashboard/StakeGraph.tsx
@@ -113,7 +113,7 @@ const StakeGraph = ({
           {graphData !== undefined ? (
             <AreaChart data={graphData}>
               <Tooltip
-                content={<CustomTooltip tooltipTitle="Volume" symbol="" />}
+                content={<CustomTooltip isPrice={false} />}
               />
               <defs>
                 <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
