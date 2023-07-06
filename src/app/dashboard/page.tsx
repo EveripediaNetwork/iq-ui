529d19c5da840328d7e2d4f137f4f6560e9299e1
Damilare Bakare <bakaredmlr@gmail.com>
2023-07-06 15:16:56 +0100

    reworks dashboard layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,6 @@ import type { NextPage } from 'next'
 import {
   Flex,
   Heading,
-  SimpleGrid,
   Stack,
   Text,
   chakra,
@@ -15,6 +14,7 @@ import {
   Spinner,
   Icon,
   Box,
+  Grid,
 } from '@chakra-ui/react'
 import { BraindaoLogo3 } from '@/components/braindao-logo-3'
 import { BraindaoLogo } from '@/components/braindao-logo'
@@ -140,154 +140,157 @@ const Home: NextPage = () => {
         />
       </Flex>
       <TokenData marketData={marketData} />
-      <SimpleGrid columns={{ base: 1, lg: 3 }} spacing={{ lg: '4' }}>
-        <GridItem
-          colSpan={[2]}
-          rounded="lg"
-          border="solid 1px "
-          borderColor="divider"
-          py={{ base: '13px', md: '22px', lg: '6' }}
-          px={{ base: '11px', md: '18px', lg: 5 }}
-        >
-          <Flex align="center">
-            <Icon as={BraindaoLogo} boxSize={7} />
-            <Text
-              fontSize={{ base: '14px', md: '21px', lg: '24px' }}
-              fontWeight="600"
-              ml="2"
-            >
-              IQ price
-            </Text>
-          </Flex>
-          <Flex mt="6px">
-            {graphData !== undefined ? (
-              <chakra.div>
-                <Text
-                  fontSize={{ base: '18px', md: '27px', lg: '30px' }}
-                  fontWeight={{ base: 700, md: '600' }}
-                >
-                  ${graphData?.[graphData.length - 1].amt.toFixed(4)}
-                </Text>
-                <chakra.span
-                  fontSize={{ base: '8px', md: '10px', lg: '12px' }}
-                  fontWeight="600"
-                  color={
-                    renderIQPercentChange()?.toString().charAt(0) === '-'
-                      ? 'red.500'
-                      : 'green'
-                  }
-                >
-                  {renderIQPercentChange()}%
-                </chakra.span>
-              </chakra.div>
-            ) : (
-              <Skeleton
-                h={{ xl: '6', base: '4' }}
-                w={{ xl: '32', base: '20' }}
-                borderRadius="full"
-              />
-            )}
-            <PriceDetails graphData={graphData} position="HIGHEST" />
-          </Flex>
-
-          <Flex
-            mt="27px"
-            sx={{
-              '.recharts-surface, .recharts-wrapper': {
-                w: 'full',
-              },
-              '.recharts-tooltip-cursor, .recharts-area-curve': {
-                color: 'brandText',
-                stroke: 'currentColor',
-              },
-              '.gradientStart': {
-                color: 'brandText',
-                _dark: {
-                  color: 'rgba(255, 26, 136, 0.2)',
-                },
-              },
-              '.gradientStop': {
-                color: 'white',
-                _dark: {
-                  color: 'transparent',
-                },
-              },
-            }}
+      <Grid templateColumns="repeat(12, 1fr)" gap={4} mb={4}>
+        <GridItem colSpan={{ base: 12, lg: 8 }}>
+          <Box
+            rounded="lg"
+            border="solid 1px "
+            borderColor="divider"
+            py={{ base: '13px', md: '22px', lg: '6' }}
+            px={{ base: '11px', md: '18px', lg: 5 }}
           >
-            <ResponsiveContainer width="100%" height={200}>
+            <Flex align="center">
+              <Icon as={BraindaoLogo} boxSize={7} />
+              <Text
+                fontSize={{ base: '14px', md: '21px', lg: '24px' }}
+                fontWeight="600"
+                ml="2"
+              >
+                IQ price
+              </Text>
+            </Flex>
+            <Flex mt="6px">
               {graphData !== undefined ? (
-                <AreaChart data={graphData}>
-                  <Tooltip content={<CustomTooltip />} />
-                  <defs>
-                    <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                      <stop
-                        className="gradientStart"
-                        offset="5%"
-                        stopColor="currentColor"
-                        stopOpacity={0.3}
-                      />
-                      <stop
-                        offset="60%"
-                        className="gradientStop"
-                        stopColor="currentColor"
-                        stopOpacity={0}
-                      />
-                    </linearGradient>
-                  </defs>
-                  <Area
-                    className="area"
-                    activeDot={{ r: 4 }}
-                    type="monotone"
-                    dataKey="amt"
-                    stroke="#FF1A88"
-                    fillOpacity={1}
-                    fill="url(#colorUv)"
-                  />
-                </AreaChart>
-              ) : (
-                <Flex
-                  direction="column"
-                  alignItems="center"
-                  justifyContent="center"
-                >
-                  <Spinner
-                    thickness="4px"
-                    speed="0.4s"
-                    color="graphSpinnerColor"
-                    emptyColor="graphSpinnerEmptyColor"
-                    size={{ xl: 'xl', base: 'md' }}
-                  />
-                  <Text mt="5" color="tooltipColor">
-                    Fetching chart data
+                <chakra.div>
+                  <Text
+                    fontSize={{ base: '18px', md: '27px', lg: '30px' }}
+                    fontWeight={{ base: 700, md: '600' }}
+                  >
+                    ${graphData?.[graphData.length - 1].amt.toFixed(4)}
                   </Text>
-                </Flex>
-              )}
-            </ResponsiveContainer>
-          </Flex>
-          <Flex>
-            <PriceDetails graphData={graphData} position="LOWEST" />
-          </Flex>
-          <Flex
-            mt={{ md: '6px' }}
-            gap={{ base: '6', md: '10', lg: '12' }}
-            {...getRootProps()}
-          >
-            {GRAPH_PERIODS.map((btn) => {
-              return (
-                <GraphPeriodButton
-                  key={btn.period}
-                  label={btn.label}
-                  {...getRadioProps({ value: btn.period })}
+                  <chakra.span
+                    fontSize={{ base: '8px', md: '10px', lg: '12px' }}
+                    fontWeight="600"
+                    color={
+                      renderIQPercentChange()?.toString().charAt(0) === '-'
+                        ? 'red.500'
+                        : 'green'
+                    }
+                  >
+                    {renderIQPercentChange()}%
+                  </chakra.span>
+                </chakra.div>
+              ) : (
+                <Skeleton
+                  h={{ xl: '6', base: '4' }}
+                  w={{ xl: '32', base: '20' }}
+                  borderRadius="full"
                 />
-              )
-            })}
-          </Flex>
+              )}
+              <PriceDetails graphData={graphData} position="HIGHEST" />
+            </Flex>
+
+            <Flex
+              mt="27px"
+              sx={{
+                '.recharts-surface, .recharts-wrapper': {
+                  w: 'full',
+                },
+                '.recharts-tooltip-cursor, .recharts-area-curve': {
+                  color: 'brandText',
+                  stroke: 'currentColor',
+                },
+                '.gradientStart': {
+                  color: 'brandText',
+                  _dark: {
+                    color: 'rgba(255, 26, 136, 0.2)',
+                  },
+                },
+                '.gradientStop': {
+                  color: 'white',
+                  _dark: {
+                    color: 'transparent',
+                  },
+                },
+              }}
+            >
+              <ResponsiveContainer width="100%" height={200}>
+                {graphData !== undefined ? (
+                  <AreaChart data={graphData}>
+                    <Tooltip content={<CustomTooltip />} />
+                    <defs>
+                      <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                        <stop
+                          className="gradientStart"
+                          offset="5%"
+                          stopColor="currentColor"
+                          stopOpacity={0.3}
+                        />
+                        <stop
+                          offset="60%"
+                          className="gradientStop"
+                          stopColor="currentColor"
+                          stopOpacity={0}
+                        />
+                      </linearGradient>
+                    </defs>
+                    <Area
+                      className="area"
+                      activeDot={{ r: 4 }}
+                      type="monotone"
+                      dataKey="amt"
+                      stroke="#FF1A88"
+                      fillOpacity={1}
+                      fill="url(#colorUv)"
+                    />
+                  </AreaChart>
+                ) : (
+                  <Flex
+                    direction="column"
+                    alignItems="center"
+                    justifyContent="center"
+                  >
+                    <Spinner
+                      thickness="4px"
+                      speed="0.4s"
+                      color="graphSpinnerColor"
+                      emptyColor="graphSpinnerEmptyColor"
+                      size={{ xl: 'xl', base: 'md' }}
+                    />
+                    <Text mt="5" color="tooltipColor">
+                      Fetching chart data
+                    </Text>
+                  </Flex>
+                )}
+              </ResponsiveContainer>
+            </Flex>
+            <Flex>
+              <PriceDetails graphData={graphData} position="LOWEST" />
+            </Flex>
+            <Flex
+              mt={{ md: '6px' }}
+              gap={{ base: '6', md: '10', lg: '12' }}
+              {...getRootProps()}
+            >
+              {GRAPH_PERIODS.map((btn) => {
+                return (
+                  <GraphPeriodButton
+                    key={btn.period}
+                    label={btn.label}
+                    {...getRadioProps({ value: btn.period })}
+                  />
+                )
+              })}
+            </Flex>
+          </Box>
+          <Box my={6}>
+            <StakeGraph />
+          </Box>
         </GridItem>
-        <TokenSupplyData tvl={tvl} totalHiiqSupply={totalHiiqSupply} />
-      </SimpleGrid>
-      <Box>
-        <StakeGraph />
-      </Box>
+        <GridItem colSpan={{ base: 12, lg: 4 }}>
+          <TokenSupplyData tvl={tvl} totalHiiqSupply={totalHiiqSupply} />
+        </GridItem>
+      </Grid>
     </Stack>
   )
 }
