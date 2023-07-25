d3a37ff9f44678e4e14006607d83b9b6b18a0a92
OleanjiKingCode <holamilean56@gmail.com>
2023-07-25 09:58:10 +0100

    improved the text UI in treasury graph

--- src/components/dashboard/TreasuryGraphTable.tsx
+++ src/components/dashboard/TreasuryGraphTable.tsx
@@ -64,7 +64,7 @@ export const TreasuryGraphTable = ({
   })
 
   const formatPieData = (data: TreasuryTokenType[], platformValue: number) => {
-    const result = data?.map(tok => ({
+    const result = data?.map((tok) => ({
       name: TOKENS[tok.id].name,
       value: (tok.raw_dollar / platformValue) * 100,
       amount: tok.raw_dollar,
@@ -132,7 +132,7 @@ export const TreasuryGraphTable = ({
                       <Td>
                         {typeof token.token === 'number'
                           ? Humanize.formatNumber(token.token, 2)
-                          : token.token.map(t => (
+                          : token.token.map((t) => (
                               <>
                                 <span>{`${formatValue(t.amount)} ${
                                   t.symbol
