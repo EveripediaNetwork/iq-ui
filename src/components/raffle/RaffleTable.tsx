ae74028f5ba9f02afebc4a3d7f19d9e5a90861bb
OleanjiKingCode <holamilean56@gmail.com>
2023-07-30 16:49:15 +0100

    rearranging files

--- src/components/raffle/RaffleTable.tsx
+++ src/components/raffle/RaffleTable.tsx
@@ -9,7 +9,7 @@ const RaffleTable = ({ filteredDetails, isShortened }: RaffleTableType) => {
     <Table fontWeight="semibold">
       <Thead border="none" bg="cardBg2">
         <Tr>
-          {['Address', 'No of raffles won'].map(column => (
+          {['Address', 'No of raffles won'].map((column) => (
             <Th
               border="none"
               whiteSpace="nowrap"
@@ -26,7 +26,7 @@ const RaffleTable = ({ filteredDetails, isShortened }: RaffleTableType) => {
         </Tr>
       </Thead>
       <Tbody>
-        {filteredDetails.map(r => (
+        {filteredDetails.map((r) => (
           <Tr whiteSpace="nowrap" key={r.address}>
             <Td fontSize="sm" color="tooltipColor" border="none">
               <Flex align="center" gap="18px" fontWeight="medium">
