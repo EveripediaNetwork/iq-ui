063bc8f21428ec0f91089ee8873d3670e7e675c9
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-08-23 09:55:44 +0100

    add ChartType

--- src/types/chartType.ts
+++ src/types/chartType.ts
@@ -1,5 +1,8 @@
 import { PieProps } from 'recharts'
 
+export type ConstantType = { [key: string]: number } | undefined
+
+
 export type OnPieEnter = NonNullable<PieProps['onMouseEnter']>
 
 export type ChartDataType = {
@@ -8,14 +11,38 @@ export type ChartDataType = {
 }
 
 interface ChartConstantTreasury {
-  light: string
-  dark: string
+   "light": string
+    "dark": string
 }
 
 interface ChartConstantNonTreasuryType {
   [key: string]: ChartConstantTreasury
 }
 
-export type ChartConstantType =
-  | ChartConstantTreasury[]
-  | ChartConstantNonTreasuryType
+// export type ChartConstantType =
+//   | ChartConstantTreasury[]
+//   | ChartConstantNonTreasuryType
+
+export type ChartType = 
+| {
+  chartData: ChartDataType[],
+  boxSize: ConstantType,
+  spacing: ConstantType,
+  radius: ConstantType,
+  colorMode: string,
+  onPieEnter?: OnPieEnter,
+  activeIndex?: number,
+  CHART_COLORS: ChartConstantNonTreasuryType,
+  isTreasuryPage: false
+}
+| {
+  chartData: ChartDataType[],
+  boxSize: ConstantType,
+  spacing: ConstantType,
+  radius: ConstantType,
+  colorMode: string,
+  onPieEnter?: OnPieEnter,
+  activeIndex?: number,
+  CHART_COLORS: ChartConstantTreasury[],
+  isTreasuryPage: true
+}
