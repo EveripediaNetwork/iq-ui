e80a68d9198bb6833a13767b7de0612179148cbd
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-08-28 12:40:38 +0100

    formatting

--- src/utils/getTokenHoldersCount.ts
+++ src/utils/getTokenHoldersCount.ts
@@ -1,5 +1,7 @@
-export const getTokenHoldersCount = async():Promise<number>=>{
-  const res = await fetch("https://api.ethplorer.io/getTokenInfo/0x579CEa1889991f68aCc35Ff5c3dd0621fF29b0C9?apiKey=freekey")
+export const getTokenHoldersCount = async (): Promise<number> => {
+  const res = await fetch(
+    'https://api.ethplorer.io/getTokenInfo/0x579CEa1889991f68aCc35Ff5c3dd0621fF29b0C9?apiKey=freekey',
+  )
   const data = await res.json()
   return data.holdersCount
 }
