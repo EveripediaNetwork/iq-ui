e4b6bf06b31007d120add4f7c37570b90a735161
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-09-05 14:56:59 +0100

    handle error from ethplorer api

--- src/utils/getTokenHoldersCount.ts
+++ src/utils/getTokenHoldersCount.ts
@@ -1,7 +1,16 @@
 export const getTokenHoldersCount = async (): Promise<number> => {
-  const res = await fetch(
-    'https://api.ethplorer.io/getTokenInfo/0x579CEa1889991f68aCc35Ff5c3dd0621fF29b0C9?apiKey=freekey',
-  )
-  const data = await res.json()
-  return data.holdersCount
+  try {
+    const res = await fetch(
+      'https://api.ethplorer.io/getTokenInfo/0x579CEa1889991f68aCc35Ff5c3dd0621fF29b0C9?apiKey=freekey',
+    )
+    const data = await res.json()
+    if (!data || typeof data.holdersCount !== 'number') {
+      throw new Error('Invalid data type received from the API')
+    }
+    return data.holdersCount
+  } catch (error) {
+    throw new Error(
+      `Error fetching token holders count: ${(error as Error).message}`,
+    )
+  }
 }
