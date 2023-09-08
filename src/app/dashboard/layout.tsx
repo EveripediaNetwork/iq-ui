d767f557a83d54b5c5aac2b12113475c810afd01
Adebesin Cell <adebesintolulope80@gmail.com>
2023-09-08 20:16:23 +0100

    [Metadata] Add metadata for BrainDAO website SEO and social media sharing.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -12,6 +12,7 @@ import { UALProviderSwitch, WalletProvider } from '@/context/eosWalletContext'
 import { DashboardLayout } from '@/components/dashboard/layout'
 import GoogleAnalyticsScripts from '@/components/SEO/GoogleAnalyticsScripts'
 import chakraTheme from '@/theme'
+import { Metadata } from 'next'
 
 const { ToastContainer } = createStandaloneToast()
 const ReduxProvider = ReduxProviderClass as (props: Dict) => JSX.Element
@@ -22,6 +23,42 @@ const client = createConfig({
   publicClient,
   webSocketPublicClient,
 })
+
+export const metadata: Metadata = {
+  title: {
+    default: 'BrainDAO | native DAO and treasury of the IQ Token',
+    template: '%s | BrainDAO',
+  },
+  description:
+    'BrainDAO is the native DAO and treasury of the IQ Token which powers IQ.wiki',
+  openGraph: {
+    title: 'BrainDAO | native DAO and treasury of the IQ Token',
+    description:
+      'BrainDAO is the native DAO and treasury of the IQ Token which powers IQ.wiki',
+    url: 'https://iq.braindao.org',
+    siteName: 'IQ Dashboard',
+    images: [
+      {
+        url: 'https://iq.braindao.org/og_image.png',
+        alt: 'BrainDAO | native DAO and treasury of the IQ Token',
+      },
+    ],
+  },
+  twitter: {
+    title: 'BrainDAO | native DAO and treasury of the IQ Token',
+    description:
+      'BrainDAO is the native DAO and treasury of the IQ Token which powers IQ.wiki',
+    site: '@Everipedia',
+    creator: '@Everipedia',
+    images: [
+      {
+        url: 'https://iq.braindao.org/og_image.png',
+        alt: 'BrainDAO | native DAO and treasury of the IQ Token',
+      },
+    ],
+    card: 'summary_large_image',
+  },
+}
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
