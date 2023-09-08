2479ba5a3fba3c95a11af7b3c635d3c3c95460a6
Adebesin Cell <adebesintolulope80@gmail.com>
2023-09-08 20:26:18 +0100

    Update metadata and open graph tags for BrainDAO dashboard

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -7,34 +7,26 @@ export const metadata: Metadata = {
     default: 'BrainDAO | native DAO and treasury of the IQ Token',
     template: '%s | BrainDAO',
   },
+  metadataBase: new URL('https://iqgpt.com'),
   description:
     'BrainDAO is the native DAO and treasury of the IQ Token which powers IQ.wiki',
   openGraph: {
     title: 'BrainDAO | native DAO and treasury of the IQ Token',
     description:
       'BrainDAO is the native DAO and treasury of the IQ Token which powers IQ.wiki',
-    url: 'https://iq.braindao.org',
+    url: 'https://iq.braindao.org/dashboard',
     siteName: 'IQ Dashboard',
+    type: 'website',
     images: [
       {
         url: 'https://iq.braindao.org/og_image.png',
-        alt: 'BrainDAO | native DAO and treasury of the IQ Token',
       },
     ],
   },
   twitter: {
-    title: 'BrainDAO | native DAO and treasury of the IQ Token',
-    description:
-      'BrainDAO is the native DAO and treasury of the IQ Token which powers IQ.wiki',
-    site: '@Everipedia',
-    creator: '@Everipedia',
-    images: [
-      {
-        url: 'https://iq.braindao.org/og_image.png',
-        alt: 'BrainDAO | native DAO and treasury of the IQ Token',
-      },
-    ],
     card: 'summary_large_image',
+    creator: '@IQWiki',
+    site: 'IQ GPT',
   },
 }
 
