6ce928f9885ee4571a57301b0594a4112a124f52
Adebesin Cell <adebesintolulope80@gmail.com>
2023-09-09 07:14:12 +0100

    Update metadataBase URL to point to the new BrainDAO website. This patch is necessary because the previous URL is outdated and needs to be updated to reflect the correct URL for the BrainDAO website.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
     default: 'BrainDAO | native DAO and treasury of the IQ Token',
     template: '%s | BrainDAO',
   },
-  metadataBase: new URL('https://iqgpt.com'),
+  metadataBase: new URL('https://iq.braindao.org/'),
   description:
     'BrainDAO is the native DAO and treasury of the IQ Token which powers IQ.wiki',
   openGraph: {
@@ -34,35 +34,37 @@ export const metadata: Metadata = {
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <meta charSet="UTF-8" />
-      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
-      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-      <meta
-        property='og:image'
-        content='https://iq.braindao.org/og_image.png'
-      />
-      <meta name='image:type' content='png' />
-      <meta
-        property='twitter:image'
-        content='https://iq.braindao.org/og_image.png'
-      />
-      <meta name='twitter:image:type' content='png' />
-      <meta name='twitter:image:width' content='300px' />
-      <meta name='twitter:image:height' content='157px' />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
-      <meta
-        name="debank-cloud-site-verification"
-        content="bba105af03355b417ae9969b16131dea"
-      />
-      <meta
-        name="apple-mobile-web-app-status-bar-style"
-        content="black-translucent"
-      />
-      <link rel="manifest" href="/manifest.json" />
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
-      />
+      <head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+        <meta
+          property='og:image'
+          content='https://iq.braindao.org/og_image.png'
+        />
+        <meta name='image:type' content='png' />
+        <meta
+          property='twitter:image'
+          content='https://iq.braindao.org/og_image.png'
+        />
+        <meta name='twitter:image:type' content='png' />
+        <meta name='twitter:image:width' content='300px' />
+        <meta name='twitter:image:height' content='157px' />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="debank-cloud-site-verification"
+          content="bba105af03355b417ae9969b16131dea"
+        />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap"
+        />
+      </head>
       <body>
         <ColorMode />
         <AppProviders>{children}</AppProviders>
