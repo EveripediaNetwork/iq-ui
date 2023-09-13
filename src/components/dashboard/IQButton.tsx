315b5f3df96270b87a8bf2a59dcb83eee723bb16
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-09-13 09:57:11 +0530

    add IQ price to button

--- src/components/dashboard/IQButton.tsx
+++ src/components/dashboard/IQButton.tsx
@@ -1,12 +1,13 @@
-import { Box, Button } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button, Tooltip } from '@chakra-ui/react'
+import React, { useState } from 'react'
 import { addToken } from '@/utils/add-new-token'
 import Image from 'next/image'
 import { fetchPrices } from '@/utils/dashboard-utils'
+import * as Humanize from 'humanize-plus'
 
 export const IQButton = () => {
-  fetchPrices().then((res) => console.log(res))
-
+  const [price, setPrice] = useState(0)
+  fetchPrices().then((res) => setPrice(res[0].prices?.[0][1]))
   return (
     <>
       <Box
@@ -15,6 +16,7 @@ export const IQButton = () => {
         as={Button}
         variant="outline"
         size="sm"
+        fontWeight="500"
         sx={{
           span: {
             gap: '2',
@@ -23,12 +25,24 @@ export const IQButton = () => {
           },
         }}
       >
-        <Image
-          src="/images/braindao-logo-4.svg"
-          alt='IQ'
-          width={25}
-          height={21}
-        />
+        <Tooltip
+          hasArrow
+          label={'Add IQ to Metamask'}
+          placement="bottom"
+          color="grayText4"
+          rounded="lg"
+          bg="tooltipBg"
+        >
+          <Box as='span'>
+            <Image
+              src="/images/braindao-logo-4.svg"
+              alt='IQ'
+              width={25}
+              height={21}
+            />
+            <Box>{`${Humanize.formatNumber(price, 4)}`}</Box>
+          </Box>
+        </Tooltip>
       </Box>
     </>
   )
