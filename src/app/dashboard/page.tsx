9ab7114f61778df4ea51bf4b05a87d349cee575d
Damilare Bakare <bakaredmlr@gmail.com>
2023-09-13 19:24:32 +0100

    fixes issues with debank values not inline with treasury value

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -82,7 +82,7 @@ const Home: NextPage = () => {
     amt: parseFloat(dt.amount),
     name: new Date(dt.created).toISOString().slice(0, 10),
   }))
-  console.log("holderValue", holderValue)
+  console.log('holderValue', holderValue)
   const { data: holderData } = useGetIQHoldersQuery(holderValue as string)
   const [prices, setPrices] = useState<Dict<Dict<number>[]> | null>(null)
   const [marketData, setMarketData] = useState<Dict | null>(null)
@@ -231,7 +231,7 @@ const Home: NextPage = () => {
                   <GraphPeriodButton
                     key={btn.period}
                     label={btn.label}
-                    {...getRadioProps({ value: btn.period })}
+                    {...getRadioProps({ value: btn.period })}  
                   />
                 )
               })}
