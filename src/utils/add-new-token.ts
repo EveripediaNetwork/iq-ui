af3824e2e6cf1d9943c717834f57e6185b8b733d
sidharthK2 <sidharthkapoor.100@gmail.com>
2023-09-13 13:50:17 +0530

    formatting

--- src/utils/add-new-token.ts
+++ src/utils/add-new-token.ts
@@ -13,9 +13,10 @@ const tokenOptionData: { [key: string]: tokenOptions } = {
     address: config.iqAddress,
     decimals: 18,
     symbol: 'IQ',
-    image: 'https://assets.coingecko.com/coins/images/5010/small/YAIS3fUh.png?1626267646',
+    image:
+      'https://assets.coingecko.com/coins/images/5010/small/YAIS3fUh.png?1626267646',
   },
-  HiIQ: { 
+  HiIQ: {
     address: config.hiiqAddress,
     decimals: 18,
     symbol: 'HiIQ',
