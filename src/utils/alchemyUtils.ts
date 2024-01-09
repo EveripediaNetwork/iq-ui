082c40b048f810d5beee144cb244927ab9501e06
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-01-09 15:23:10 +0530

    Remove unused getPriceDetailsBySymbol function

--- src/utils/alchemyUtils.ts
+++ src/utils/alchemyUtils.ts
@@ -1,6 +1,4 @@
 import { Alchemy } from 'alchemy-sdk'
-import { getError } from './getError'
-import axios from 'axios'
 
 export const fetchContractBalances = async (
   alchemyInstance: Alchemy,
@@ -24,17 +22,17 @@ export const getTokenDetails = async (contractAddress: string) => {
   return tokenDetails.json()
 }
 
-export const getPriceDetailsBySymbol = async (symbol: string) => {
-  try {
-    const res = await axios.get('/api/cmc-token-details', {
-      params: { tokenName: symbol },
-    })
-    const response = res.data
-    const { data } = response.response
-    const tokenDetails = data[symbol]
-    return tokenDetails?.quote?.USD?.price
-  } catch (error) {
-    console.log('Error fetching data:', getError(error))
-    return null
-  }
-}
+// export const getPriceDetailsBySymbol = async (symbol: string) => {
+//   try {
+//     const res = await axios.get('/api/cmc-token-details', {
+//       params: { tokenName: symbol },
+//     })
+//     const response = res.data
+//     const { data } = response.response
+//     const tokenDetails = data[symbol]
+//     return tokenDetails?.quote?.USD?.price
+//   } catch (error) {
+//     console.log('Error fetching data:', getError(error))
+//     return null
+//   }
+// }
