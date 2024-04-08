750ffafbe12f927cc0cf37a1e816572a0182c6c3
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-04-08 22:02:22 +0100

    removing redundant logic

--- src/hooks/useErc20.ts
+++ src/hooks/useErc20.ts
@@ -4,22 +4,18 @@ import { useAccount, useBalance } from 'wagmi'
 export const useErc20 = () => {
   const { address } = useAccount()
 
-  const shouldFetchBalance = !!address
-
   const { data: erc20Balance, refetch: refetchErc20Balance } = useBalance({
     address: address,
     token: config.iqAddress as `0x${string}`,
-    enabled: shouldFetchBalance,
   })
 
   const { data: totalValueLocked } = useBalance({
     address: config.hiiqAddress as `0x${string}`,
     token: config.iqAddress as `0x${string}`,
-    enabled: shouldFetchBalance,
   })
 
   const getUserBalance = () => {
-    if (shouldFetchBalance) {
+    if (address) {
       refetchErc20Balance()
     }
     return erc20Balance?.value ?? BigInt(0)
