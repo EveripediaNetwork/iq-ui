69212c757e334d3fef5647ebb97b614cb2ae3999
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-04-16 15:43:27 +0100

    Add NotFound component

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import ErrorPage from '@/components/client/404'
+
+export default function NotFound() {
+  return (
+    <html lang="en">
+      <body>
+        <ErrorPage />
+      </body>
+    </html>
+  )
+}
