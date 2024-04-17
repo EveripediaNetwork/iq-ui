3190eda3ff093b4cd6517d89947a46ceb51fc703
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-04-17 11:08:38 +0100

    Add metadata for 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,6 +4,31 @@ import ColorMode from '@/components/chakra/ColorMode'
 import ErrorPage from '@/components/client/404'
 import { ChakraProvider } from '@chakra-ui/react'
 import chakraTheme from '@/theme'
+import { Metadata } from 'next'
+
+const notFoundTitle = 'Opps! Page Not Found'
+const notFoundMessage =
+  "Sorry, but the page you're looking for doesn't exist. Don't worry, we'll help you find your way back."
+
+export const metadata: Metadata = {
+  title: notFoundTitle,
+  description: notFoundMessage,
+  metadataBase: new URL('https://iq.braindao.org/dashboard'),
+  openGraph: {
+    title: notFoundTitle,
+    description: notFoundMessage,
+    type: 'website',
+    siteName: 'IQ Dashboard',
+  },
+  twitter: {
+    title: notFoundTitle,
+    description: notFoundMessage,
+    images: 'https://iq.braindao.org/images/og-image.png',
+    card: 'summary_large_image',
+    creator: '@IQWiki',
+    site: 'IQ Dashbaord',
+  },
+}
 
 export default function NotFound() {
   return (
