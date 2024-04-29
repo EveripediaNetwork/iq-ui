a8e239544a9b0689b7690a71315c6951e7c63af9
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-04-29 17:49:37 +0100

    refactor wagmi config

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,10 +1,13 @@
-import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect'
-import { MetaMaskConnector } from '@wagmi/core/connectors/metaMask'
-import { mainnet } from 'wagmi'
-import { configureChains } from '@wagmi/core'
-import { goerli } from 'wagmi/chains'
-import { alchemyProvider } from '@wagmi/core/providers/alchemy'
-import { publicProvider } from '@wagmi/core/providers/public'
+// import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect'
+// import { MetaMaskConnector } from '@wagmi/core/connectors/metaMask'
+//TODO: uncomment import { coinbaseWallet, injected } from 'wagmi/connectors'
+// import { mainnet, } from 'wagmi'
+//TODO uncomment import { mainnet } from 'wagmi/chains'
+//TODO: ***Continue wagmi v2 migration and tenderly integration need to replace alchemy provider */
+// import { configureChains } from '@wagmi/core'
+// import { goerli } from 'wagmi/chains'
+// import { alchemyProvider } from '@wagmi/core/providers/alchemy'
+// import { publicProvider } from '@wagmi/core/providers/public'
 
 import config from './index'
 
