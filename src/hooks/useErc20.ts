0a7cc2817e43d7c2186c9fe7f91cefad39cc354a
SidharthK2 <sidharthkapoor.100@gmail.com>
2024-05-09 10:06:21 +0100

    Update useErc20 hook to support WIQ on dev

--- src/hooks/useErc20.ts
+++ src/hooks/useErc20.ts
@@ -3,16 +3,27 @@ import { useAccount, useBalance } from 'wagmi'
 
 export const useErc20 = () => {
   const { address } = useAccount()
+  const { data: erc20Balance, refetch: refetchErc20Balance } =
+    config.alchemyChain === 'iqChain'
+      ? useBalance({
+          address: address,
+          token: config.wiqAddress as `0x${string}`,
+        })
+      : useBalance({
+          address: address,
+          token: config.iqAddress as `0x${string}`,
+        })
 
-  const { data: erc20Balance, refetch: refetchErc20Balance } = useBalance({
-    address: address,
-    token: config.iqAddress as `0x${string}`,
-  })
-
-  const { data: totalValueLocked } = useBalance({
-    address: config.hiiqAddress as `0x${string}`,
-    token: config.iqAddress as `0x${string}`,
-  })
+  const { data: totalValueLocked } =
+    config.alchemyChain === 'iqChain'
+      ? useBalance({
+          address: config.hiiqAddress as `0x${string}`,
+          token: config.wiqAddress as `0x${string}`,
+        })
+      : useBalance({
+          address: config.hiiqAddress as `0x${string}`,
+          token: config.iqAddress as `0x${string}`,
+        })
 
   const getUserBalance = () => {
     if (address) {
