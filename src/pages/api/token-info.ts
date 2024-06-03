9aefd360e6d43a5f8fa1c21b6968636d7a586f5c
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-06-03 22:29:43 +0100

    further iterations on code climate error

--- src/pages/api/token-info.ts
+++ src/pages/api/token-info.ts
@@ -1,6 +1,6 @@
-import config from '@/config'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { ResponseData } from '@/types/TreasuryTokenType'
+import { fetchDataFromAPI } from '@/utils/fetchData.utils'
 
 export default async function handler(
   req: NextApiRequest,
@@ -13,17 +13,7 @@ export default async function handler(
       message: 'List of Token address and chain id are needed',
     })
   }
+
   const url = `https://pro-openapi.debank.com/v1/token/list_by_ids?chain_id=${chain}&ids=${ids}`
-  const result = await fetch(url, {
-    headers: {
-      Accept: 'application/json',
-      Accesskey: `${config.debankApiKey}`,
-    },
-  })
-  res.setHeader('Cache-Control', 's-maxage=43200')
-  return res.status(200).json({
-    response: await result.json(),
-    status: true,
-    message: 'list of token information successfully fetched',
-  })
+  await fetchDataFromAPI(url, res)
 }
