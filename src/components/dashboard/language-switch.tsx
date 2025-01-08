35dd5b004dbf2891ee6eac8f934f524a808d025d
yomzy2223 <oluwolesayo10@gmail.com>
2025-01-08 20:31:32 +0100

    update locale switch compoenent to include other languages

--- src/components/dashboard/language-switch.tsx
+++ src/components/dashboard/language-switch.tsx
@@ -8,11 +8,46 @@ import {
   Box,
   BoxProps,
   Image,
+  Text,
 } from '@chakra-ui/react'
+import { useLocale } from 'next-intl'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { FaChevronDown } from 'react-icons/fa'
 
+export const locales = [
+  {
+    locale: 'en',
+    default: true,
+    name: 'English',
+    currency: 'usd',
+    localization: 'en-US',
+    icon: '/US.svg',
+  },
+  {
+    locale: 'kr',
+    name: 'Korean',
+    currency: 'krw',
+    localization: 'ko-KR',
+    icon: '/KR.svg',
+  },
+  {
+    locale: 'zh',
+    name: 'Chinese',
+    currency: 'cny',
+    localization: 'zh-CN',
+    icon: '/ZH.svg',
+  },
+] as const
+
 export const LanguageSwitch = (props: BoxProps) => {
+  const pathname = usePathname()
+  const locale = useLocale()
+
+  const currentLocale = locales.find((loc) => loc.locale === locale)
+  const strippedPathname = pathname?.replace(`/${locale}`, '') || '/'
+
   return (
     <Box {...props} minW="fit-content">
       <Menu>
@@ -29,25 +64,31 @@ export const LanguageSwitch = (props: BoxProps) => {
             },
           }}
         >
-          <Image
-            src="/images/usa-flag.png"
-            objectFit="cover"
-            boxSize="32px"
-            alt="🇺🇸"
-          />
-          <Box display={{ base: 'none', sm: 'inherit' }}>ENG</Box>
+          <Text>{currentLocale?.locale.toUpperCase()}</Text>
           <Icon as={FaChevronDown} fontSize="sm" />
         </MenuButton>
+
         <MenuList>
-          <MenuItem gap="4">
-            <Image
-              src="/images/usa-flag.png"
-              objectFit="cover"
-              boxSize="32px"
-              alt="🇺🇸"
-            />
-            ENG
-          </MenuItem>
+          {locales.map((locale) => (
+            <MenuItem gap="4">
+              <Link
+                locale={locale.locale}
+                href={strippedPathname}
+                className="flex items-center gap-2"
+                aria-label={`Change language to ${locale.name}`}
+              >
+                <Image
+                  src={locale.icon}
+                  alt={locale.name}
+                  width={24}
+                  height={24}
+                  aria-hidden="true"
+                  className="max-w-6 max-h-6"
+                />
+                <span>{locale.name}</span>
+              </Link>
+            </MenuItem>
+          ))}
         </MenuList>
       </Menu>
     </Box>
