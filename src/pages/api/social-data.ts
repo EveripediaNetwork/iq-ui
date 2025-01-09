0e729b5aa9487dd1aa4ce66342fa0d291ac63210
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-01-09 10:03:58 +0100

    add social-data api route

---
+++ src/pages/api/social-data.ts
@@ -0,0 +1,56 @@
+import { setCacheHeaders } from '@/utils/cache'
+import { NextApiRequest, NextApiResponse } from 'next/types'
+
+const CACHE_DURATION_SECONDS = 12 * 60 * 60 // 12 hours
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', ['GET'])
+    res.status(405).end(`Method ${req.method} Not Allowed`)
+    return
+  }
+
+  try {
+    const redditResponse = await fetch(
+      'https://www.reddit.com/r/everipedia/about.json',
+    )
+    const twitterResponse = await fetch(
+      'https://twitter-api45.p.rapidapi.com/followers.php?screenname=IQWIKI',
+      {
+        headers: {
+          'x-rapidapi-key':
+            '141412b455mshd09467584258910p14bf05jsn949a29def245',
+          'x-rapidapi-host': 'twitter-api45.p.rapidapi.com',
+          'cache-control': `public, s-maxage=${CACHE_DURATION_SECONDS}, stale-while-revalidate=${
+            2 * CACHE_DURATION_SECONDS
+          }`,
+        },
+      },
+    )
+    const { followers_count: twitterFollowers } = await twitterResponse.json()
+    const redditData = await redditResponse.json()
+    const data = {
+      social: {
+        twitter: twitterFollowers,
+        reddit: redditData.data.subscribers,
+      },
+    }
+
+    setCacheHeaders(res)
+    res.status(200).json(data)
+  } catch (error) {
+    console.error('Fetch social data error:', error)
+    res
+      .status(500)
+      .json({
+        status: false,
+        message:
+          error instanceof Error
+            ? error.message
+            : 'Failed to fetch social data',
+      })
+  }
+}
