9514f5e7815cbd4af4aca60e785feedc5a6a41f8
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-01-14 15:01:58 +0100

    refactor setCacheHeaders

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -4,8 +4,11 @@ import { NextApiResponse } from 'next'
 export const setCacheHeaders = (
   res: NextApiResponse,
   cacheHeader = `public, s-maxage=${config.CACHE_DURATION_SECONDS}, stale-while-revalidate=${config.CACHE_STALE_WHILE_REVALIDATE_SECONDS}`,
-) => {
-  res.setHeader('Cache-Control', cacheHeader)
-  res.setHeader('CDN-Cache-Control', cacheHeader)
-  res.setHeader('Vercel-CDN-Cache-Control', cacheHeader)
+): void => {
+  const headers = [
+    ['Cache-Control', cacheHeader],
+    ['CDN-Cache-Control', cacheHeader],
+    ['Vercel-CDN-Cache-Control', cacheHeader],
+  ] as const
+  headers.forEach(([name, value]) => res.setHeader(name, value))
 }
