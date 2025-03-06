e51ba1164e0b6a4267b18c89866a385ddeae99e5
Aliu Salaudeen <ayokunumi99@gmail.com>
2025-03-06 10:15:45 +0100

    rename IQGPT to AIDEN

--- src/components/aiden-logo.tsx
+++ src/components/aiden-logo.tsx
@@ -1,7 +1,7 @@
 import { Icon, IconProps } from '@chakra-ui/react'
 import React from 'react'
 
-export const IqgptLogo = (props: IconProps) => {
+export const AIDENLogo = (props: IconProps) => {
   return (
     <Icon
       xmlns="http://www.w3.org/2000/svg"
